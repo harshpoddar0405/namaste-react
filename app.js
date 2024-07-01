eaf49a0bf0b3f16a4038692b195eb25d0c52d70f
harshpoddar0405 <poddarharsh14@gmail.com>
2024-07-02 02:26:04 +0530

    commit

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ import ReactDOM from "react-dom/client";
 
 //React element
 
+// React.createElement => Object => HTMLElement(render)
+
 const heading =React.createElement(
   "h1",
   {id: "heading"},
    "Namaste React"
 );
+
+console.log(heading);
+
+//JSX is not HTML inside javascript it is HTML/XML like syntax 
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX </h1>
+
+console.log(jsxHeading);  
+
+
 
 const  root = ReactDOM.createRoot(document.getElementById("root"));
 
