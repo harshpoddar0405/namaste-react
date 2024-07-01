e06b0a9b2c59a5cc0b9bef50be201b6124422eb9
harshpoddar0405 <poddarharsh14@gmail.com>
2024-07-02 03:35:24 +0530

    commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 
 //React element
 
-// React.createElement => Object => HTMLElement(render)
+// React.createElement =>ReactElement-JS Object => HTMLElement(render)
 
 const heading =React.createElement(
   "h1",
@@ -15,13 +15,16 @@ const heading =React.createElement(
 console.log(heading);
 
 //JSX is not HTML inside javascript it is HTML/XML like syntax 
+//JSX code is transpiled before it reaches the JS engine by parcel using babel(library)
+//JSX => React.createElement =>ReactElement-JS Object => HTMLElement(render) using babel
+//attributes in jsx are written in camelCase
+//If JSX is written in multiple lines in should be wrapped inside round brackets if written in one line brackets are not needed 
 
-const jsxHeading = <h1 id="heading">Namaste React using JSX </h1>
+const jsxHeading = <h1 className= "head">Namaste React using JSX </h1>
 
 console.log(jsxHeading);  
 
-
-
 const  root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
+ 
